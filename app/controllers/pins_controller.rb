7fe36eca70f653cc0b837ad1f936708092083582
Terry Hsiang <terryhsiang@gmail.com>
2015-11-06 00:23:18 -0800

    Add Image upload with paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show] 
   def index
     @pins = Pin.all
   end
@@ -49,7 +49,7 @@ class PinsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
+       def pin_params
       params.require(:pin).permit(:description, :image)
-    end
+      end
 end
